a796ff6873dcd23184fe58b16e48d52e9f06e40f
lamazi <maciek@emplawyer.pl>
2016-08-05 19:55:59 +0000

    zmiana sqlite na posgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160731112402) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "teams", force: :cascade do |t|
     t.string   "nazwa"
     t.string   "kategoria"
